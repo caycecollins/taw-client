26ae9cb66faec2a9c7ed70958914a57feecc6f6e
Cayce Collins <cayce.collins@gmail.com>
2017-07-09 13:52:38 -0700

    initial work on games flexbox layout and added css transitions to avoid async content popping onto page before view transitions have completed

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { connect } from 'cerebral/react'
 import { state, signal } from 'cerebral/tags'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 import ViewContainer from '../ViewContainer'
 import Link from '../Link'
@@ -10,21 +11,31 @@ import Link from '../Link'
 function Games ({ gamesList }) {
   return (
     <ViewContainer>
-      Game Divisions
-      <br />
-      <br />
-      {gamesList && gamesList.map((game, index) => {
-        return (
-          <Link
-            key={game.id}
-            routeTo="game"
-            routeParams={{ id: JSON.stringify(game.id) }}
-            label={game.name}
-          >
-          </Link>
-        )
-      })}
-
+      <ViewHeader>
+        Game Divisions
+      </ViewHeader>
+      <CSSTransitionGroup
+        transitionName="initial"
+        transitionAppear={true}
+        transitionAppearTimeout={1000}
+        transitionEnter={false}
+        transitionLeave={false}
+        component="span"
+      >
+        <GamesContainer>
+          {gamesList && gamesList.map((game, index) => {
+            return (
+              <Game
+                key={game.id}
+                routeTo="game"
+                routeParams={{ id: JSON.stringify(game.id) }}
+              >
+                <Name>{game.name}</Name>
+              </Game>
+            )
+          })}
+        </GamesContainer>
+      </CSSTransitionGroup>
     </ViewContainer>
   )
 }
@@ -39,3 +50,38 @@ export default connect(
   },
   Games
 )
+
+const ViewHeader = styled.div`
+  margin: 24px 0;
+  font-size: 24px;
+  padding-left: 40px;
+  text-transform: uppercase;
+  color: ${props => props.theme.colors.lightTan};
+`
+
+const GamesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 24px;
+`
+
+const Game = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 150px;
+  height: 150px;
+  margin: 16px;
+  padding: 16px;
+  background-color: rgba(0,0,0,.3);
+  &:hover {
+    background-color: rgba(0,0,0,.6);
+  }
+`
+
+const Name = styled.div`
+  text-align: center;
+`
