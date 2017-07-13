1c2b36e9b5d89bfd5455d28554da926113a64853
Cayce Collins <cayce.collins@gmail.com>
2017-07-13 02:22:25 -0700

    clearing more state on logout, removed localStorage chaching for user (still stores it, just doesn't use it instead of the api upon login)

--- src/modules/user/index.js
+++ src/modules/user/index.js
@@ -1,18 +1,21 @@
 import changeView from '../../factories/changeView'
 
 const getUser = async ({ path, http, state, storage }) => {
-  const userFromLocalStorage = storage.get('user')
+  // const userFromLocalStorage = storage.get('user')
   const tokenFromLocalStorage = storage.get('authorization.token')
   if (!tokenFromLocalStorage) {
     return path.redirected()
   }
-  if (userFromLocalStorage && userFromLocalStorage.callsign) {
-    state.set('user', userFromLocalStorage)
-  } else {
-    const getUserDataFromApi = await http.get('/users/me')
-    state.set('user', getUserDataFromApi.result)
-    storage.set('user', getUserDataFromApi.result)
-  }
+  // if (userFromLocalStorage && userFromLocalStorage.callsign) {
+  //   state.set('user', userFromLocalStorage)
+  // } else {
+  //   const getUserDataFromApi = await http.get('/users/me')
+  //   state.set('user', getUserDataFromApi.result)
+  //   storage.set('user', getUserDataFromApi.result)
+  // }
+  const getUserDataFromApi = await http.get('/users/me')
+  state.set('user', getUserDataFromApi.result)
+  storage.set('user', getUserDataFromApi.result)
   return path.continue()
 }
 
