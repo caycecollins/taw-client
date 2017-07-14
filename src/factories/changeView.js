170b12524856b64ba2b1fc500a883d65d9595271
Cayce Collins <cayce.collins@gmail.com>
2017-07-13 17:52:20 -0700

    close sidebar when primary navigation occurs

--- src/factories/changeView.js
+++ src/factories/changeView.js
@@ -3,6 +3,7 @@ import { state } from 'cerebral/tags'
 
 export default (view, continuesequence = []) => {
   return [
+    set(state`app.sidebarActive`, null),
     set(state`app.previousView`, state`app.currentView`),
     set(state`app.currentView`, view),
     continuesequence,
