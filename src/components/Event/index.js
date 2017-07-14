7af01bcc1ccb7e69ddb7169b5d1a17b16c64ef06
Cayce Collins <cayce.collins@gmail.com>
2017-07-13 17:47:40 -0700

    viewing an event now converts to the user's timezone

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -3,28 +3,41 @@ import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import { connect } from 'cerebral/react'
 import { signal, state } from 'cerebral/tags'
-import moment from 'moment'
-import { rgba } from 'polished'
+import moment from 'moment-timezone'
 
 function EventModal (props) {
+  const tableData = [
+    ['title', props.event.title],
+    ['start', moment.tz(props.event.start, props.userTimezone).format('dddd, MMMM Do YYYY @ HHmm (h:mm a) z')],
+    ['end', moment.tz(props.event.end, props.userTimezone).format('dddd, MMMM Do YYYY @ HHmm (h:mm a) z')],
+  ]
+
   return (
     <EventContainer>
-      {props.event &&
-        Object.keys(props.event).map((key, index) => {
-          const val = props.event[key].toString()
-          return (
-            <div key={index}>
-              {key}: {val}
-            </div>
-          )
-        })
-      }
+      <Section>
+        Details
+        <br />
+        <br />
+        <table>
+          <tbody>
+            {tableData.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item[0].toUpperCase()}</td>
+                  <td>{item[1]}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </Section>
     </EventContainer>
   )
 }
 
 EventModal.propTypes = {
   event: PropTypes.object,
+  userTimezone: PropTypes.string,
 }
 
 EventModal.defaultProps = {
@@ -34,6 +47,7 @@ EventModal.defaultProps = {
 export default connect(
   {
     event: state`event`,
+    userTimezone: state`user.timezone`,
   },
   EventModal
 )
@@ -43,4 +57,21 @@ const EventContainer = styled.div`
   flex-direction: column;
   font-sizeE: 0.9rem;
   color: ${props => props.theme.colors.armyWhite};
+`
+
+const Section = styled.div`
+  margin-bottom: 24px;
+  > table {
+    tbody {
+      tr {
+        height: 40px;
+        td {
+          &:first-of-type {
+            padding-right: 24px;
+            font-size: 0.95rem;
+          }
+        }
+      }
+    }
+  }
 `
