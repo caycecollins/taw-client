76c741a3e4585532bea23b0982b0b593737aebd4
Cayce Collins <cayce.collins@gmail.com>
2017-07-19 12:56:57 -0700

    using css grid for games divisions view :allthethings:

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -89,11 +89,9 @@ const ViewHeader = styled.div`
 
 const GamesContainer = styled.div`
   > div {
-    display: flex;
-    flex: 1;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
+    display: grid;
+    grid-gap: 24px;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
     transition: all .3s cubic-bezier(.4,0,.2,1);
     @media (max-width: 768px) {
       justify-content: center;
@@ -105,10 +103,8 @@ const Game = styled(Link)`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-wrap: wrap;
   width: 150px;
   height: 150px;
-  margin: 16px 32px 16px 0;
   padding: 16px;
   background-color: rgba(0,0,0,.3);
   transition: all .3s cubic-bezier(.4,0,.2,1);
