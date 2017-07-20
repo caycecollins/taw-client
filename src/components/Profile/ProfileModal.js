5eee031b12493009bdba933564425040ae5d8bbf
Cayce Collins <cayce.collins@gmail.com>
2017-07-20 02:24:07 -0700

    Profile edit component refactored to use new form standards

--- src/components/Profile/ProfileModal.js
+++ src/components/Profile/ProfileModal.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { connect } from 'cerebral/react'
 import { signal, state } from 'cerebral/tags'
-import { form } from '@cerebral/forms'
 
+import Form from '../Form'
+import SubmitButton from '../Form/SubmitButton'
 import Button from '../Button'
 import Input from '../Input'
 import timezonesFromJson from '../Events/timezones.json'
+
+const formPath = 'profile.editForm'
 
 const timezones = timezonesFromJson.map((timezone, index) => {
   if (!timezone.utc) return false
@@ -23,7 +26,6 @@ const timezones = timezonesFromJson.map((timezone, index) => {
 
 timezones.unshift(<option key="empty" value="">Select your timezone</option>)
 
-const getFields = props => Object.keys(props.form.getFields()) || []
 const getUserTimezoneText = userTimezone => timezonesFromJson.filter(tz => tz.utc && tz.utc[0] === userTimezone)
 
 const EditProfile = props => {
@@ -36,61 +38,32 @@ const EditProfile = props => {
       <br />
       Current user timezone: {getUserTimezoneText(props.userTimezone)[0].text}
       <br />
-      <form onSubmit={(e) => e.preventDefault()}>
-        {props.userTimezone && getFields(props).map((field, index) => {
-          if (props.form[field].type === 'select') {
-            return (
-              <Input
-                type="select"
-                value={props.userTimezone}
-                name={field}
-                key={index}
-                path={`profile.editForm.${field}`}
-              >
-                {field === 'timezone' && timezones}
-              </Input>
-            )
-          }
-          return (
-            <Input
-              type={props.form[field].type}
-              name={field}
-              key={index}
-              path={`profile.editForm.${field}`}
-            />
-          )
-        })}
+      <Form>
+        <Input
+          type="select"
+          value={props.userTimezone}
+          label="timezone"
+          path={`${formPath}.timezone`}
+        >
+          {timezones}
+        </Input>
         <br />
         <br />
         <Button
-          onClick={e => props.resetForm({ formPath: 'profile.editForm' })}
+          onClick={e => props.resetForm({ formPath })}
           label="Reset"
           type="button"
         />
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <Button
-          onClick={e => {
-            e.preventDefault()
-            !props.form.isPristine && props.form.isValid && props.profileUpdated()
-          }}
-          disabled={!props.form.isValid || props.profileUpdating}
-          label={props.profileUpdating ? 'Saving...' : 'Save'}
-          type="submit"
-          icon={props.profileUpdating && 'crosshairs'}
-          iconSpin={props.profileUpdating && true}
-        />
-      </form>
+        <SubmitButton form={formPath} />
+      </Form>
     </EditProfileContainer>
   )
 }
 
 EditProfile.propTypes = {
-  form: PropTypes.object,
   userTimezone: PropTypes.string,
-  profileUpdating: PropTypes.bool,
-  profileUpdated: PropTypes.func,
   resetForm: PropTypes.func,
-  setFieldDefaultValue: PropTypes.func,
 }
 
 EditProfile.defaultProps = {
@@ -98,13 +71,8 @@ EditProfile.defaultProps = {
 
 export default connect(
   {
-    form: form(state`profile.editForm`),
     userTimezone: state`user.timezone`,
-    userTimezoneText: state`user.timezoneText`,
-    profileUpdating: state`profile.updating`,
-    profileUpdated: signal`profile.updated`,
     resetForm: signal`app.onReset`,
-    setFieldDefaultValue: signal`app.setFieldDefaultValue`,
   },
   EditProfile
 )
