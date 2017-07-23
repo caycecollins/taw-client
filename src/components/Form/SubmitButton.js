d859ceb6301ff82240c9167626a6d725390e0e5a
Cayce Collins <cayce.collins@gmail.com>
2017-07-22 20:42:34 -0700

    refactored SubmitButton component (new sidebar actions deprecated SubmitButton component kinda, however... it will still be used in other places)

--- src/components/Form/SubmitButton.js
+++ src/components/Form/SubmitButton.js
@@ -7,21 +7,21 @@ import Button from '../Button'
 
 const handleSubmitForm = (props, e) => {
   e.preventDefault()
-  props.form.isValid && props.submitForm({ form: props.formProp })
+  props.form.isValid && props.submitForm({ form: props.formPath })
 }
 
 const SubmitButton = connect(
   {
-    form: form(state`${props`form`}`),
-    formProp: props`form`,
-    submitForm: signal`${props`form`}Submitted`,
-    pendingResponse: state`${props`form`}.pending`,
+    form: form(state`${props`formPath`}`),
+    formPath: props`formPath`,
+    submitForm: signal`${props`submitSignal`}`,
+    pendingResponse: state`${props`pendingState`}`,
   },
   props =>
     <Button
       onClick={e => handleSubmitForm(props, e)}
       disabled={!props.form.isValid || props.pendingResponse}
-      label={props.pendingResponse ? 'Saving...' : 'Save'}
+      label={props.pendingResponse ? props.pendingLabel || 'Saving...' : props.label || 'Save'}
       type="submit"
       icon={props.pendingResponse && 'crosshairs'}
       iconSpin={props.pendingResponse && true}
