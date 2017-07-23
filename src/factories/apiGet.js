dc9c38e60d326ffaed5617e69db16a620fb394fe
Cayce Collins <cayce.collins@gmail.com>
2017-07-22 20:03:38 -0700

    apiGet factory housekeeping

--- src/factories/apiGet.js
+++ src/factories/apiGet.js
@@ -1,20 +1,7 @@
-export default function apiGetFactory (endpoint, pathCheck, requiredCheck) {
+export default function apiGetFactory (endpoint, pathCheck) {
   const apiGet = async ({ path, http, state, storage, router }) => {
     if (!state.get('authorization.authenticated')) return path.error()
     if (!endpoint || !pathCheck) return path.error({ error: 'endpoint and path required' })
-    const requiredPathFromLocalStorage = storage.get(requiredCheck)
-    if (requiredCheck && !requiredPathFromLocalStorage) {
-      return path.error({ error: 'big error' })
-    }
-    // const pathFromLocalStorage = storage.get(pathCheck)
-    // if (pathFromLocalStorages) {
-    //   state.set(pathCheck, pathFromLocalStorage)
-    //   return path.success({ result: pathFromLocalStorage })
-    // } else {
-    //   const getDataFromApi = await http.get(endpoint)
-    //   storage.set(pathCheck, getDataFromApi.result)
-    //   return path.success({ result: getDataFromApi.result })
-    // }
     const getDataFromApi = await http.get(endpoint)
     state.set(pathCheck, getDataFromApi.result)
     storage.set(pathCheck, getDataFromApi.result)
