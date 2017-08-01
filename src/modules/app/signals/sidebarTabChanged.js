dc366df40ada71c8a78c88205dbefa6d20fd817b
Cayce Collins <cayce.collins@gmail.com>
2017-08-01 02:49:04 -0700

    cleaned up logic of sidebarTabChanged (use a single when operator to control both outcomes)

--- src/modules/app/signals/sidebarTabChanged.js
+++ src/modules/app/signals/sidebarTabChanged.js
@@ -2,9 +2,10 @@ import { when, set } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
 
 export default [
-  set(state`app.sidebarTab`, props`tab`),
-  when(state`app.sidebarTab`), {
-    true: [],
+  when(props`tab`), {
+    true: [
+      set(state`app.sidebarTab`, props`tab`),
+    ],
     false: [
       set(state`app.sidebarTab`, null),
     ],
