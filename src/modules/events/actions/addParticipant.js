0a617fcecd0187ae6a58fdf2953148899aa3f5a7
Cayce Collins <cayce.collins@gmail.com>
2017-08-01 21:40:17 -0700

    fixed issue with adding participants not properly updating search results and in some cases duplicates were being allowed (also fixed)

--- src/modules/events/actions/addParticipant.js
+++ src/modules/events/actions/addParticipant.js
@@ -1,14 +1,16 @@
 import { findIndex } from 'lodash'
 
 export default function addParticipant ({ props, state }) {
-  const searchResultIndex = findIndex(state.get('search.results'), result => result.id === props.participant.key)
-  if (searchResultIndex >= 0) state.set(`search.results.${searchResultIndex}.exists`, true)
-
-  state.push(`${props.path}.participants`, {
-    display: props.participant.value,
-    model: {
-      [props.participant.type === 'user' ? 'callsign' : 'name']: props.participant.value,
-      id: props.participant.key,
-    },
-  })
+  const checkParticipants = findIndex(state.get(`${props.path}.participants`), participant => participant.model.id === props.participant.key)
+  if (checkParticipants < 0) {
+    const searchResultIndex = findIndex(state.get(`search.results`), result => result.id === props.participant.key)
+    state.set(`search.results.${searchResultIndex}.exists`, true)
+    state.push(`${props.path}.participants`, {
+      display: props.participant.value,
+      model: {
+        [props.participant.type === 'user' ? 'callsign' : 'name']: props.participant.value,
+        id: props.participant.key,
+      },
+    })
+  }
 }
