be069dc910dd7b62c5249b54cb6542fc7fabb5b5
Cayce Collins <cayce.collins@gmail.com>
2017-08-01 21:30:10 -0700

    moved getRecurringInfo into separate action

--- src/modules/events/actions/scheduleEvent.js
+++ src/modules/events/actions/scheduleEvent.js
@@ -1,16 +1,6 @@
 import moment from 'moment'
 
-const getRecurringInfo = weekly => {
-  const daysOfWeek = []
-  if (weekly.sun && weekly.sun.value) daysOfWeek.push(0)
-  if (weekly.mon && weekly.mon.value) daysOfWeek.push(1)
-  if (weekly.tue && weekly.tue.value) daysOfWeek.push(2)
-  if (weekly.wed && weekly.wed.value) daysOfWeek.push(3)
-  if (weekly.thu && weekly.thu.value) daysOfWeek.push(4)
-  if (weekly.fri && weekly.fri.value) daysOfWeek.push(5)
-  if (weekly.sat && weekly.sat.value) daysOfWeek.push(6)
-  return daysOfWeek
-}
+import getRecurringInfo from '../helpers/getRecurringInfo'
 
 export default function scheduleEvent ({ controller, props, state, http, forms }) {
   state.set(`${props.form}.pending`, true)
