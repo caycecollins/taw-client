02d23c5b6782efb9694c43518b3ef664f174539d
Cayce Collins <cayce.collins@gmail.com>
2017-08-03 02:14:07 -0700

    re-fetch events upon edit event submission, added custom reset after scheduling an event

--- src/modules/events/signals/editEventSubmitted.js
+++ src/modules/events/signals/editEventSubmitted.js
@@ -1,5 +1,7 @@
+import apiGet from '../../../factories/apiGet'
 import editEvent from '../actions/editEvent'
 
 export default [
   editEvent,
+  apiGet('/events', 'events.eventsData'), { success: [], error: [] },
 ]
--- src/modules/events/signals/scheduleEventSubmitted.js
+++ src/modules/events/signals/scheduleEventSubmitted.js
@@ -1,5 +1,10 @@
+import { wait } from 'cerebral/operators'
+
 import scheduleEvent from '../actions/scheduleEvent'
+import scheduleEventReset from '../signals/scheduleEventReset'
 
 export default [
   scheduleEvent,
+  wait(400),
+  scheduleEventReset,
 ]
